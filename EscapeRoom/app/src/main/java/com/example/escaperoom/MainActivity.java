d4f97bedb3d60686cd3027aa14e269402988391e
federico29mg <65299467+federico29mg@users.noreply.github.com>
2020-11-03 19:13:03 -0500

    Conexion

--- EscapeRoom/app/src/main/java/com/example/escaperoom/MainActivity.java
+++ EscapeRoom/app/src/main/java/com/example/escaperoom/MainActivity.java
@@ -11,6 +11,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.StringReader;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
     static final String CORREO = "chzdiane22@gmail.com";
@@ -32,21 +44,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void comenzarJuego(View view) {
-        String correoIngresado = et_Email.getText().toString().trim();
-        String contrasenaIngresada = et_Contraseña.getText().toString();
+        validarUsuario();
+    }
 
-        if(correoIngresado.equals("") || contrasenaIngresada.equals("")) {
-            Toast toast = Toast.makeText(getApplicationContext(), "Complete datos de ingreso", Toast.LENGTH_LONG);
-            toast.show();
-        }
-        else if(correoIngresado.equals(CORREO) && contrasenaIngresada.equals(CONTRASENA)) {
-            Intent intent = new Intent(getApplicationContext(), Tematica.class);
-            startActivity(intent);
-        }
-        else {
-            Toast toast = Toast.makeText(getApplicationContext(), "Datos de ingreso erroneos", Toast.LENGTH_LONG);
-            toast.show();
-        }
+    private void validarUsuario() {
+        String URL = "https://192.168.1.1//bdescaperooms/validar_usuario.php";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if(!response.isEmpty()) {
+                    Intent intent = new Intent(getApplicationContext(), Tematica.class);
+                    startActivity(intent);
+                }
+                else {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Datos de ingreso erroneos", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_LONG).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parametros = new HashMap<String, String>();
+                parametros.put("correo_electronico", et_Email.getText().toString());
+                parametros.put("contrasena", et_Contraseña.getText().toString());
+                return parametros;
+            }
+        };
 
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    public void conectar(View view) {
+        validarUsuario();
     }
 }
