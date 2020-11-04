6346f09511ac45bba30fb6167ef5eee952382957
SebasG25 <bitas200225@gmail.com>
2020-11-04 07:33:51 -0500

    Validar login (Api)

--- EscapeRoom/app/src/main/java/com/example/escaperoom/MainActivity.java
+++ EscapeRoom/app/src/main/java/com/example/escaperoom/MainActivity.java
@@ -6,9 +6,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -28,7 +31,10 @@ public class MainActivity extends AppCompatActivity {
     static final String CORREO = "chzdiane22@gmail.com";
     static final String CONTRASENA = "123abc";
     EditText et_Email, et_Contraseña;
+    TextView tvRegistro;
     Button b_Continuar;
+    String url = "http://192.168.1.3/bdEscapeRooms/validar_usuario.php";
+    SpannableString mitextoU = new SpannableString("Regístrate");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +47,10 @@ public class MainActivity extends AppCompatActivity {
         et_Email = findViewById(R.id.et_Email);
         et_Contraseña = findViewById(R.id.et_Contraseña);
         b_Continuar = findViewById(R.id.b_Continuar);
+        tvRegistro = findViewById(R.id.tvRegistro);
+        SpannableString mitextoU = new SpannableString("Regístrate");
+        mitextoU.setSpan(new UnderlineSpan(),0, mitextoU.length(), 0);
+        tvRegistro.setText(mitextoU);
     }
 
     public void comenzarJuego(View view) {
@@ -48,10 +58,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void validarUsuario() {
-        String URL = "https://192.168.1.1//bdescaperooms/validar_usuario.php";
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+        RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
+
                 if(!response.isEmpty()) {
                     Intent intent = new Intent(getApplicationContext(), Tematica.class);
                     startActivity(intent);
@@ -76,11 +87,17 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
         requestQueue.add(stringRequest);
     }
 
     public void conectar(View view) {
         validarUsuario();
+    }
+
+    public void Registro(View view) {
+        //Crear layout de registro
+        Intent intent = new Intent(getApplicationContext(), Tematica.class);
+        startActivity(intent);
     }
 }
